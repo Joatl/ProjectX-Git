a78efe64803ee5d6b8fcf50b8b0c2e89343ac8b1
Joatl <32833420+Joatl@users.noreply.github.com>
2018-10-24 13:16:23 +0200

    sans les cles

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -5,7 +5,7 @@ Created on Tue May  8 16:53:49 2018
 @author: jattal
 """
 
-Consumer_key='zswJgttcXG2eoyqCEch2VHudy'
-Consumer_secret='jTf1eyxNQ8qX3jbjnzAIYxP0LuFz7SfSHNY3cj01BD7Fa5MwfL'
-Access_token = '988317193900380160-DfafkCc3SE6FkRzzagMvBumdGAIKu4s'
-Access_secret = 'HJa3pvkoq3kQgvHgtylcNTKw4vr9bBOSmO6kmxflGTvkz'
+Consumer_key=xx
+Consumer_secret=xx
+Access_token = xx
+Access_secret = xx
